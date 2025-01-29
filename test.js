fc9057b3dbc1171f29b86d9e1fc0a5991ef542a2
manishroy09 <manishroy2163@gmail.com>
2025-01-30 02:49:09 +0530

    30th jan 2025

---
+++ test.js
@@ -0,0 +1,53 @@
+// input fild  ,button color 
+
+
+
+function fun(){
+    let color = document.querySelector('#input').value;
+    document.body.style.backgroundColor = color
+    return false;
+}
+
+
+//Q2 increment , decriment by 5, reset 
+
+
+let v = 0
+function inc(){
+    
+      document.querySelector('#value').innerHTML = v+5
+   
+ }
+
+ function dec(){
+    
+    document.querySelector('#value').innerHTML = v-5
+     
+ }
+
+ function reset(){
+  
+      document.querySelector('#value').innerHTML = 0
+      
+ }
+
+//q3 gulab jamun , kajukatli, malaibarfi image will print on image box
+
+
+function gulab(){
+    let img = document.getElementById('image')
+    img.src = 'gulab.jpeg'
+    
+}
+
+function kaju(){
+    let img = document.getElementById('image')
+    img.src = 'kaju.jpeg'
+   
+}
+
+function malai(){
+    let img = document.getElementById('image')
+    img.src = 'malai.jpeg'
+    
+}
